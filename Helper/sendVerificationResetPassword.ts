44b51a8a5a3a1c07dd5fa94699aba5c520e31430
Mo'men <mmm.isam.99@gmail.com>
2026-03-15 15:30:16 +0200

    delete nodemailer and using brevo library

--- Helper/sendVerificationResetPassword.ts
+++ Helper/sendVerificationResetPassword.ts
@@ -1,34 +1,27 @@
-import nodemailer from "nodemailer";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
-
+import { BrevoClient } from "@getbrevo/brevo";
 import dotenv from "dotenv";
 
 dotenv.config();
 
+const brevo = new BrevoClient({
+  apiKey: process.env.BREVO_API_KEY as string,
+});
+
 export const sendVerificationResetPassword = async (
   email: string,
   token: string,
-  id:string
+  id: string
 ) => {
-  const transportOptions = {
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  } as SMTPTransport.Options;
-
   const verificationLink = `${process.env.BASE_FRONT_URL}/password/verify-email/${id}/${token}`;
-  const transporter = nodemailer.createTransport(transportOptions);
 
-  await transporter.sendMail({
-    from: `"e-Commerce App" <${process.env.EMAIL_USER}>`,
-    to: email,
+  await brevo.transactionalEmails.sendTransacEmail({
+    sender: {
+      email: process.env.BREVO_SENDER_EMAIL as string,
+      name: "e-Commerce App",
+    },
+    to: [{ email }],
     subject: "Verify your email",
-    html: `
+    htmlContent: `
       <h2>Email Verification</h2>
       <p>Please click the link below to verify your email:</p>
       <a href="${verificationLink}">Verify Email</a>
