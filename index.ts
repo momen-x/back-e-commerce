363157f40f81ccf4332a4e0decab056121f6472a
Mo'men <mmm.isam.99@gmail.com>
2026-03-15 12:12:50 +0200

    update the cors middleware

--- index.ts
+++ index.ts
@@ -1,54 +1,50 @@
-import express,{Request,Response} from "express";
+import express, { Request, Response } from "express";
 import { connectDB } from "./Config/DBConnect";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRoute from "./Modules/User/Routes/User";
 import authRoute from "./Modules/User/Auth/Routes/Auth";
 import categoryRoute from "./Modules/Category/Routes/Category";
-import ProductsRoute from "./Modules/Products/Routes/Products"
-import OrderItemRoute from "./Modules/Order_Items/Routes/Order_items"
-import OrderRoute from "./Modules/Order/Routes/Order"
+import ProductsRoute from "./Modules/Products/Routes/Products";
+import OrderItemRoute from "./Modules/Order_Items/Routes/Order_items";
+import OrderRoute from "./Modules/Order/Routes/Order";
 import paymentRoute from "./Modules/Payment/Routes/Payment";
-import { errorHandler, notFound } from './middlewares/err';
+import { errorHandler, notFound } from "./middlewares/err";
 import cors from "cors";
 dotenv.config();
 
-
 connectDB();
-const app=express();
+const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://back-e-commerce-iyvz.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
-  })
+  }),
 );
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req:Request,res:Response)=>{
-    res.send("hello world");
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello world");
 });
-app.use("/api/users",userRoute);
-app.use("/api/users/auth",authRoute);
-app.use("/api/categories",categoryRoute);
-app.use("/api/order-items",OrderItemRoute);
-app.use("/api/products",ProductsRoute);
-app.use("/api/orders",OrderRoute);
+app.use("/api/users", userRoute);
+app.use("/api/users/auth", authRoute);
+app.use("/api/categories", categoryRoute);
+app.use("/api/order-items", OrderItemRoute);
+app.use("/api/products", ProductsRoute);
+app.use("/api/orders", OrderRoute);
 app.use("/api/payment", paymentRoute);
 
 //error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
+const PORT = process.env.PORT || 5000;
 
-
-
-const PORT=process.env.PORT || 5000;
-
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
-
-
